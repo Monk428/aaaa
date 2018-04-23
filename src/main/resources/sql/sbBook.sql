f03a0ee26f260917fe7a25bda47ebd759aa4aa22
wengjianfeng <464493571@qq.com>
2018-04-23 14:26:07 +0800

    修改数据库

--- src/main/resources/sql/sbBook.sql
+++ src/main/resources/sql/sbBook.sql
@@ -11,11 +11,24 @@
  Target Server Version : 50721
  File Encoding         : utf-8
 
- Date: 04/21/2018 17:45:49 PM
+ Date: 04/23/2018 14:25:22 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `login_ticket`
+-- ----------------------------
+DROP TABLE IF EXISTS `login_ticket`;
+CREATE TABLE `login_ticket` (
+  `t_id` int(32) NOT NULL AUTO_INCREMENT,
+  `t_userId` int(32) DEFAULT NULL,
+  `t_expired` date DEFAULT NULL,
+  `t_status` int(1) DEFAULT NULL,
+  `t_ticket` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`t_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 --  Table structure for `user`
@@ -27,6 +40,9 @@ CREATE TABLE `user` (
   `t_password` varchar(32) NOT NULL,
   `t_email` varchar(32) DEFAULT NULL,
   `t_mobile` varchar(32) DEFAULT NULL,
+  `t_salt` varchar(32) DEFAULT NULL,
+  `t_headUrl` varchar(32) DEFAULT NULL,
+  `t_role` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`t_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
